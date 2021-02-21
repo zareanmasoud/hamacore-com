dac7dcc2f9cb8b4968b40b7ebdfd53c364540703
Sanaz <mahmoudisanaz59@gmail.com>
2021-02-21 15:33:30 +0330

    Updates

---
+++ precache-manifest.46186b290dfeb012628faf3261e64db7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7603ffa42199a0b7c28750a6750305",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5c8793ccd1fef85a890",
+    "url": "./static/css/main.d6d4e16c.chunk.css"
+  },
+  {
+    "revision": "39ef88b1d207fd36024e",
+    "url": "./static/js/2.346a3097.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "./static/js/2.346a3097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5c8793ccd1fef85a890",
+    "url": "./static/js/main.123af276.chunk.js"
+  },
+  {
+    "revision": "a7771d93065ff4888484",
+    "url": "./static/js/runtime-main.9c5ee330.js"
+  },
+  {
+    "revision": "f0e3035ba319ef5b31e1a4a00186baea",
+    "url": "./static/media/bg4.f0e3035b.jpg"
+  },
+  {
+    "revision": "1667372b330a977665eb9f3fe3525f89",
+    "url": "./static/media/logo.1667372b.png"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "./static/media/privacy-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "fc55445889a41f2e1eb72e7ab9f04cfe",
+    "url": "./static/media/screenshot0.fc554458.png"
+  },
+  {
+    "revision": "53a2c4dc90bf1f939c579cbe1b23c91d",
+    "url": "./static/media/screenshot1.53a2c4dc.png"
+  },
+  {
+    "revision": "d98f4830b612e35831f34e117768cad2",
+    "url": "./static/media/screenshot2.d98f4830.png"
+  },
+  {
+    "revision": "c89c67d0a734079d7cdf5320cfcd2e0e",
+    "url": "./static/media/screenshot3.c89c67d0.png"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "./static/media/support-bg.df8fd3ef.jpg"
+  }
+]);
